c44244799257727402d3324f4b0204a6c1aa9bfd
EmmaPanerai <131193591+EmmaPanerai@users.noreply.github.com>
2023-04-18 19:44:29 +0200

    modification texte page

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,10 @@ const App = () => {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Bienvenue sur le blind test</h1>
+        <h1 className="App-title">Bienvenue sur le blind test codé par Emma Cap'imtaine Panerai</h1>
       </header>
       <div className="App-images">
-        <p>Blind test codé par Emma Cap'imtaine Panerai ! </p>
+        <p>Tente de deviner le plus possible de musique ! À toi de jouer !</p>
       </div>
       <div className="App-buttons"></div>
     </div>
